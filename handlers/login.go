1a74b289977a1352ca71345c144941586995d3d1
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-17 01:22:51 +0900

    ログインを途中まで作成（できていな）

--- handlers/login.go
+++ handlers/login.go
@@ -2,8 +2,19 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/garyburd/go-oauth/oauth"
 )
 
+var loginOauthClient = oauth.Client{
+	TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+	ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authenticate",
+	TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
+	callback := "http://" + r.host + "/callback"
+	tempCred, err := loginOauthClient.RequestTemporaryCredentials(nil,callback,nil)
+
 	//todo
 }
