19a94a9f6ed6d25d0a328eb5af34cef2979f7adb
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-17 22:49:51 +0900

    セッション管理を簡潔に

--- session/session.go
+++ session/session.go
@@ -1,25 +1,15 @@
 package session
 
-//https://astaxie.gitbooks.io/build-web-application-with-golang/ja/06.2.htmlを参考にさせていただいています。
-import "sync"
+import (
+	"sync"
+	"time"
+)
+var mu sync.Mutex
+var sessions = make (map[string]Session)
 
-type Manager struct{
-	cookiName string
-	lock sync.Mutex
-	provider Provider
-	liveingTime int64
+
+type Session struct{
+	dbid int64
+	crateAt time.Time
 }
 
-type Provider interface {
-	SessionInit(sid string) (Session,error)//sessionの初期化を実装か
-	SessionRead(sid string) (Session, error)//sidが示すセッションを変数を返すもしなければ新しく作る
-	SessionDestroy(sid string) error//sessionを削除
-	SessionGarbageCollection(maxLifeTime int64)//時間切れのセッションをお掃除
-}
-
-type Session interface {
-	Set(key, value interface{}) error //valueをセット
-	Get(key interface{}) interface{}  //valueを取得
-	Delete(key interface{}) error     //kyeにもとずいて削除
-	SessionID() string                //sidを取得
-}
