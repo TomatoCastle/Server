7a209948ca4b011fe7a1fa8001fed30224fab4bc
tomato <nomu.tomato.zyusu@gmail.com>
2018-03-18 17:34:29 +0900

    getを肉好き

--- session/session.go
+++ session/session.go
@@ -3,13 +3,35 @@ package session
 import (
 	"sync"
 	"time"
+	"net/http"
+	"fmt"
 )
-var mu sync.Mutex
-var sessions = make (map[string]Session)
+var lock sync.Mutex
+var sessions = make (map[string]map[string]interface{})
 
-
+/*
 type Session struct{
-	dbid int64
+	userid int64
 	crateAt time.Time
+	otehr
+}*/
+/*
+type Session interface {
+
+}*/
+
+func SessionGet(w http.ResponseWriter, r *http.Request) (sess map[string]interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+	if c, err := r.Cookie("go-session");c != nil{
+		sess = sessions[c.Value]
+	}else if err != nil || c == nil {
+		sess = make(map[string]interface{})
+	}
+	return sess
 }
 
+func SessionSet(w http.ResponseWriter, r *http.Request, sessionColum string, value map[string]interface{}) error{
+//todo セッションをほぞんするの中身を追加
+	return nil
+}
